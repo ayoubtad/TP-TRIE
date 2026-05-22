f6e30626251cec9ee4435fff56cebe079cb549d7
Ayoub Tadili <ayoubtadili139@gmail.com>
2026-05-22 10:21:21 +0100

    trie par inssertion

--- index.php
+++ index.php
@@ -1,20 +1,41 @@
 <?php
-
 require_once 'tri.php';
-/*$t = [4, 5, 8, 0, 1, 1];
-echo "avant de trier : " . implode(', ', $t) . "\n";
-echo "Tri  Bulles\n";
-echo implode(', ', triBulles($t)) . "\n";
-echo "l'algoritme du tri à bulle a fait " .
+
+/*
+=============tri à bulles============
+$t = [3, 1, 6, 0, 15, 5, 9];
+$n = count($t);
+
+echo "avant de trier : " . implode(", ", $t) . "\n";
+
+echo "Tri à bulles :\n";
+
+echo implode(", ", triBulles($t)) . "\n";
+
+echo "L'algorithme tri à bulles a fait " .
     triBullesCompte($t) .
-    " comparaisons pur trier un tableau de" . count($t) . "elements";*/
+    " comparaisons pour trier un tableau de " .
+    count($t) . " éléments.\n";
+
+echo "Temps d'exécution : " . triBullesChrono($t) . " ms\n";
+
 
 $tailles = [100, 500, 1000, 2000, 5000, 10000];
 
-
 foreach ($tailles as $n) {
-    $tab = range($n, 1); // [n, n -1, ... , 2, 1] = cas defavorable
+    $tab = range($n, 1);
+
     $nbComp = triBullesCompte($tab);
     $temps = triBullesChrono($tab);
-    echo "n = $n : $nbComp comparaisons , $temps ms\n";
+    echo "n = $n : $nbComp comparaisons, $temps ms\n";
 }
+========tri par selection================
+$tab = [5, 3, 8, 1, 4];
+$trie = triSelection($tab);
+echo implode(', ', $trie); // attendu : 1, 3, 4, 5, 8
+// 
+=============tri par inserion============
+ */
+$tab = [5, 3, 8, 1, 4];
+$trie = triInsertion2($tab);
+echo implode(', ', $trie); // attendu : 1, 3, 4, 5, 8
